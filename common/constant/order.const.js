58a0aa0cc4b84078ef706bfdf0792c18b0f9b90e
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-13 02:14:29 +0330

    create order status enum

---
+++ common/constant/order.const.js
@@ -0,0 +1,13 @@
+const OrderStatus = {
+    Pending: "pending",
+    Ordered: "ordered",
+    InProcess: "in-process",
+    Packed: "packed",
+    InTransit: "in-transit",
+    Canceled: "canceled",
+    Delivery: "delivery",
+};
+
+module.exports = {
+    OrderStatus
+};
