abb4ed6ceae0d5c30a79866fdaac7920cdf0b1d7
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-19 22:49:22 +0330

    add rbac routes to app config

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const {authRoutes} = require("./modules/auth/auth.routes");
 const {basketRoutes} = require("./modules/basket/basket.routes");
 const {paymentRoutes} = require("./modules/payment/payment.routes");
 const {orderRoutes} = require("./modules/order/order.routes");
+const {rbacRoutes} = require("./modules/RBAC/rbac.routes");
 config();
 async function main () {
     const app = express();
@@ -17,6 +18,7 @@ async function main () {
     app.use("/basket", basketRoutes);
     app.use("/payment", paymentRoutes);
     app.use("/order", orderRoutes);
+    app.use("/rbac", rbacRoutes);
     app.use((req, res, next) => {
         return res.status(404).json({
             message: "not found route"
