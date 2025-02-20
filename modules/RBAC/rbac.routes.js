c4cbcb8acea1964d5b4f1e2917533f9211fdab1f
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-20 17:39:08 +0330

    create route for assing permission to role

--- modules/RBAC/rbac.routes.js
+++ modules/RBAC/rbac.routes.js
@@ -1,10 +1,12 @@
 const {Router} = require("express");
 const AuthGuard = require("../auth/auth.guard");
-const {createRoleHandler, createPermissionHandler} = require("./rbac.service");
+const {createRoleHandler, createPermissionHandler, assignPermissionToRoleHandler} = require("./rbac.service");
+const {assignPermissionToRoleValidation} = require("./validation");
 
 const router = Router();
 router.post("/role", AuthGuard, createRoleHandler);
 router.post("/permission", AuthGuard, createPermissionHandler);
+router.post("/add-permission-to-role", AuthGuard, assignPermissionToRoleValidation, assignPermissionToRoleHandler);
 module.exports = {
     rbacRoutes: router
 };
